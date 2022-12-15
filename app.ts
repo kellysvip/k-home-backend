8c831b940eb109d23d9ca4e5db0f3402858e33c9
Vu Hoang <hoangitv6@gmail.com>
2022-12-15 15:24:34 +0700

    base-code

---
+++ app.ts
@@ -0,0 +1,65 @@
+import express from "express";
+import path from "path";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+import cors from "cors";
+import router from "./routes/index";
+import { NextFunction, Request, Response } from "express";
+import mongoose from "mongoose";
+import { AppError, sendResponse } from "./helpers/ultis";
+
+require("dotenv/config");
+
+const app = express();
+
+app.use(cors());
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+// Connect to MONGODB   //NODE_ENV === 'test'
+const mongoURI: string =
+  process.env.MONGODB_URI ||
+  "mongodb+srv://admin:admin@cluster0.t244bsh.mongodb.net/k-home-test";
+
+mongoose
+  .connect(mongoURI)
+  .then(async () => console.log(`DB connected ${mongoURI}`))
+  .catch((err: string) => console.log(err));
+
+//Error Handler
+app.use((err: AppError, req: Request, res: Response, next: NextFunction) => {
+  err.statusCode = 404;
+  err.message = "Not Found";
+  next(err);
+});
+
+app.use((err: AppError, req: Request, res: Response, next: NextFunction) => {
+  console.log("ERROR", err);
+  if (err.isOperational) {
+    return sendResponse(
+      res,
+      err.statusCode ? err.statusCode : 500,
+      false,
+      { message: err.message },
+      null,
+      err.errorType
+    );
+  } else {
+    return sendResponse(
+      res,
+      err.statusCode ? err.statusCode : 500,
+      false,
+      { message: err.message },
+      null,
+      "Internal Server Error"
+    );
+  }
+});
+
+app.use("/api", router);
+
+export default app;
+module.exports = app;
---
+++ helpers/ultis.ts
@@ -0,0 +1,42 @@
+import { NextFunction, Request, Response } from "express";
+
+export function sendResponse(
+  res: Response,
+  status: number,
+  success: boolean,
+  data: Record<string, unknown >,
+  errors: Record<string, unknown> | null,
+  message: string
+) {
+  const response: {
+    success?: boolean;
+    data?: Record<string, unknown >;
+    errors?: Record<string, unknown> | null;
+    message?: string;
+  } = {};
+  if (success) response.success = success;
+  if (data) response.data = data;
+  if (errors) response.errors = errors;
+  if (message) response.message = message;
+  return res.status(status).json(response);
+}
+export const catchAsync = (func: Function) => (req: Request, res: Response, next: NextFunction ) => {
+    func(req, res, next).catch((err: Error) => res.status(500).json({errMessage: err.message}))
+}
+
+export class AppError extends Error {
+  public statusCode: number;
+  public errorType: string;
+  // all errors using this class are operational errors.
+  public isOperational = true;
+  constructor(statusCode: number, message: string, errorType: string) {
+    super(message);
+    this.statusCode = statusCode;
+    this.errorType = errorType;
+
+    // create a stack trace for debugging (Error obj, void obj to avoid stack polution)
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+
