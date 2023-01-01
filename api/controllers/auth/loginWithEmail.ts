299d5de19c5107dd275a2e3f40037fa6d054e9f3
Vu Hoang <hoangitv6@gmail.com>
2023-01-01 14:31:32 +0700

    fix auth

--- api/controllers/auth/loginWithEmail.ts
+++ api/controllers/auth/loginWithEmail.ts
@@ -6,21 +6,17 @@ import bcrypt from "bcryptjs";
 
 export const loginWithEmail = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
-    //get data from request
     const { email, password } = req.body;
-    //Validation
 
     let user = (await User.findOne({ email })) as unknown as IUser;
     if (!user) throw new AppError(400, "Invalid Credentials", "Login Error");
 
-    //Process
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) throw new AppError(400, "Wrong Password", "Login Error");
 
     const accessToken = await user.generateToken();
     console.log(accessToken);
 
-    //Response
     sendResponse(res, httpStatus.OK, { user, accessToken }, "Login Success");
   }
 );
