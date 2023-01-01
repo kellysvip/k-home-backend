98ac58e0050a1f0df2145ea855bb44c13fea8ec1
Vu Hoang <hoangitv6@gmail.com>
2023-01-02 00:04:08 +0700

    auth test fix

--- api/controllers/auth/loginWithEmail.test.ts
+++ api/controllers/auth/loginWithEmail.test.ts
@@ -26,7 +26,6 @@ describe("Users Login", () => {
       })
       .expect(200);
     expect(body).toEqual({
-      success: true,
       data: {
         user: {
           _id: "639b61b0db289c8cda7ce595",
@@ -38,7 +37,7 @@ describe("Users Login", () => {
           jobTitle: "",
           faceBookLink: "",
           instagramLink: "",
-          createdAt: expect.any(String),
+          createdAt: "2022-12-15T18:04:32.145Z",
           updatedAt: expect.any(String),
           __v: 0,
         },
@@ -57,16 +56,32 @@ describe("Users Login", () => {
     ]);
   });
 
-  // test("POST /users --> user Sunny login with wrong password", async () => {
-  //   const { body } = await request(app)
-  //     .post("/auth/login")
-  //     .send({
-  //       "email": "sunny.duong.02@gmail.com",
-  //       "password": "12345"
-  //     })
-  //     .expect(200);
-  //   expect(body).toEqual({
-  //       "errMessage": "Wrong Password"
-  //   });
-  // });
+  test("POST /users --> user Login", async () => {
+    const { body } = await request(app)
+      .post("/api/auth/login")
+      .send({
+        email: "saobien1412@gmail.com",
+        password: "123s",
+      })
+      .expect(500);
+    expect(body).toEqual({
+      errMessage: "Wrong Password",
+    });
+  });
+
+  test("POST /users --> user Login", async () => {
+    const { body } = await request(app)
+      .post("/api/auth/login")
+      .send({
+        "email": "saobien1412",
+        "password": "123s"
+    })
+      .expect(400);
+    expect(body).toEqual({
+      "data": {
+          "message": "Invalid email"
+      },
+      "message": "Validator Error"
+  });
+  });
 });
